fd9cf253f9c8c0bfe298580556be31aa6f378720
HystericLamp <159586448+HystericLamp@users.noreply.github.com>
2025-09-22 15:19:40 -0600

    Fresh SpringBoot project setup

---
+++ ecommerce-backend/src/main/java/com/ecommerce/bcruz/BcruzApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.bcruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcruzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcruzApplication.class, args);
+	}
+
+}
---
+++ ecommerce-backend/src/test/java/com/ecommerce/bcruz/BcruzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.bcruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcruzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
