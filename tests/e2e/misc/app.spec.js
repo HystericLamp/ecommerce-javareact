3190628fbdda376b16894953de59a9cf2bbfff1e
HystericLamp <159586448+HystericLamp@users.noreply.github.com>
2026-05-13 11:50:20 -0600

    Setting up e2e testing framework

--- tests/e2e/misc/app.spec.js
+++ tests/e2e/misc/app.spec.js
@@ -1,58 +1,29 @@
-// tests/home-page.spec.js
 import { test, expect } from '@playwright/test';
 
 test('"/home" page should exist', async ({ page, baseURL }) => {
   const response = await page.goto('/home', { waitUntil: 'domcontentloaded' });
 
-  // Check response
   expect(response).not.toBeNull();
   expect(response.ok()).toBeTruthy();
 
-  // check we ended up on the right URL string
   await expect(page).toHaveURL(/\/home/);
 });
 
 test('"/cart" page should exist', async ({ page, baseURL }) => {
   const response = await page.goto('/cart', { waitUntil: 'domcontentloaded' });
 
-  // Check response
   expect(response).not.toBeNull();
   expect(response.ok()).toBeTruthy();
 
-  // check we ended up on the right URL string
   await expect(page).toHaveURL(/\/cart/);
-});
-
-test('"/orderconfirmation" page should exist', async ({ page, baseURL }) => {
-  const response = await page.goto('/orderconfirmation', { waitUntil: 'domcontentloaded' });
-
-  // Check response
-  expect(response).not.toBeNull();
-  expect(response.ok()).toBeTruthy();
-
-  // check we ended up on the right URL string
-  await expect(page).toHaveURL(/\/orderconfirmation/);
-});
-
-test('"/productdetail" page should exist', async ({ page, baseURL }) => {
-  const response = await page.goto('/productdetail', { waitUntil: 'domcontentloaded' });
-
-  // Check response
-  expect(response).not.toBeNull();
-  expect(response.ok()).toBeTruthy();
-
-  // check we ended up on the right URL string
-  await expect(page).toHaveURL(/\/productdetail/);
 });
 
 test('"/shop" page should exist', async ({ page, baseURL }) => {
   const response = await page.goto('/shop', { waitUntil: 'domcontentloaded' });
 
-  // Check response
   expect(response).not.toBeNull();
   expect(response.ok()).toBeTruthy();
 
-  // check we ended up on the right URL string
   await expect(page).toHaveURL(/\/shop/);
 });
 
---
+++ tests/playwright.config.js
@@ -0,0 +1,70 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+import dotenv from 'dotenv';
+import path from 'path';
+
+// dotenv
+const envName = process.env.NODE_ENV || 'dev';
+const envFile = `.env.${envName}`;
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
+
+const baseURL = process.env.BASE_URL;
+
+// For testing & debugging
+console.log('Loading env file:', envFile);
+console.log('Resolved path:', path.resolve(process.cwd(), envFile));
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './e2e',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    baseURL,
+    headless: true,
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm run dev',
+    port: 5173,
+    reuseExistingServer: !process.env.CI,
+  },
+});
