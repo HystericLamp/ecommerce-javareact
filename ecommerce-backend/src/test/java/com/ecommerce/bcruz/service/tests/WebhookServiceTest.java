2a18b8e77648be10ea5824dc5a24df79fc2ea9a7
HystericLamp <159586448+HystericLamp@users.noreply.github.com>
2026-05-26 12:13:18 -0600

    Trying to get pipeline to pass

--- ecommerce-backend/src/test/java/com/ecommerce/bcruz/service/tests/WebhookServiceTest.java
+++ ecommerce-backend/src/test/java/com/ecommerce/bcruz/service/tests/WebhookServiceTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.ecommerce.bcruz.models.DraftOrder;
 import com.ecommerce.bcruz.models.DraftOrderItem;
@@ -25,6 +26,7 @@ import com.ecommerce.bcruz.service.WebhookService;
 import com.stripe.model.PaymentIntent;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 public class WebhookServiceTest
 {
 	@Mock
