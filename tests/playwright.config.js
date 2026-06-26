3c6f5cf6236f7455e393ac2fd8c24b143703e02a
HystericLamp <159586448+HystericLamp@users.noreply.github.com>
2026-06-26 12:16:51 -0600

    Seeing if the pipeline will work

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -41,8 +41,8 @@ export default defineConfig({
     apiBaseURL,
     headless: true,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
